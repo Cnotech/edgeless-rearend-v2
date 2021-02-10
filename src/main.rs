e802ec77449d2cf631b60338e846d78d7bf63c4d
Cno <dsyourshy@qq.com>
2021-02-10 18:36:39 +0800

    完成版本号提取器函数

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use regex::{Regex, Captures};
 
 #[get("/info/{quest}")]
 async fn factory_info(web::Path(quest): web::Path<String>) -> impl Responder {
-    let file_name = file_selector(String::from("E:\\Edgeless_Onedrive\\OneDrive - 洛阳科技职业学院\\Socket"),String::from("^Edgeless.*iso$"));
+    //let file_name = file_selector(String::from("E:\\Edgeless_Onedrive\\OneDrive - 洛阳科技职业学院\\Socket"),String::from("^Edgeless.*iso$"));
+    let file_name=version_extractor(String::from("Edgeless_Beta_3.1.0.iso"),3);
     if let Err(error) = file_name{
         return format!("Error: Internal\n{}",error);
     }
@@ -14,7 +15,6 @@ async fn factory_info(web::Path(quest): web::Path<String>) -> impl Responder {
 
 #[get("/plugin/{quest}")]
 async fn factory_plugin(web::Path(quest): web::Path<String>) -> impl Responder {
-    println!("run plugin factory");
     format!("Plugin, quest:{}", quest)
 }
 
@@ -44,7 +44,7 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-//文件扫描选择函数
+//文件选择器函数
 fn file_selector(path:String,exp:String)->Result<String,String> {
 
     //校验路径是否存在
@@ -68,12 +68,22 @@ fn file_selector(path:String,exp:String)->Result<String,String> {
     for entry in file_list.unwrap(){
         let file_name=entry.unwrap().file_name().clone();
         let true_name=file_name.to_str().unwrap();
-        println!("checking {}", &true_name);
+        //println!("checking {}", &true_name);
         if regex::is_match(&exp,true_name).unwrap(){
-            println!("match {}", &true_name);
+            //println!("match {}", &true_name);
             return Ok(String::from(true_name));
         }
     }
 
     return Err(String::from("file_selector:Matched nothing when looking into ")+&path+" for "+&exp);
+}
+
+//版本号提取器函数
+fn version_extractor(name:String,index:usize)->Result<String,String>{
+    let result:Vec<&str>=name.split("_").collect();
+    if index> result.len()-1 {
+        return Err(String::from("version_extractor:Index out of range when split ")+&name+",got "+&index.to_string());
+    }
+    println!("{:?}",result);
+    return Ok(result[index].to_string());
 }
