b78d62b525c8df0120dba3561642a25fe1187493
Cno <dsyourshy@qq.com>
2021-02-12 23:19:02 +0800

    格式化

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,68 @@
-use actix_web::{get, http::header, http::StatusCode, web, App, HttpResponse, HttpServer, Result,http::ContentEncoding};
-use regex::Regex;
-use std::{fs, path::Path,os::windows::prelude::*};
-use serde::{Deserialize, Serialize};
 use actix_web::dev::BodyEncoding;
+use actix_web::{
+    get, http::header, http::ContentEncoding, http::StatusCode, web, App, HttpResponse, HttpServer,
+    Result,
+};
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+use std::{fs, os::windows::prelude::*, path::Path};
 use textcode::gb2312;
 
 //常量配置
 const DISK_DIRECTORY: &str = "E:\\Edgeless_Onedrive\\OneDrive - 洛阳科技职业学院";
 const STATION_URL: &str = "https://pineapple.edgeless.top/disk";
-const TOKEN: &str ="WDNMD";
+const TOKEN: &str = "WDNMD";
 
 //自定义Json结构
 #[derive(Serialize, Deserialize)]
 struct CateData {
-    payload:Vec<String>
+    payload: Vec<String>,
 }
 #[derive(Serialize, Deserialize)]
 struct ListData {
-    payload:Vec<ListObj>
+    payload: Vec<ListObj>,
 }
 #[derive(Serialize, Deserialize)]
 struct ListObj {
-    name:String,
-    size:u64,
-    node_type:String,
-    url:String
+    name: String,
+    size: u64,
+    node_type: String,
+    url: String,
 }
 
 //自定义请求参数结构体
 #[derive(Deserialize)]
 struct EptAddrQueryStruct {
     name: String,
-    cate:String,
-    version:String,
-    author:String
+    cate: String,
+    version: String,
+    author: String,
 }
 #[derive(Deserialize)]
 struct PluginListQueryStruct {
-    name:String
+    name: String,
 }
 #[derive(Deserialize)]
-struct TokenRequiredQueryStruct{
-    token:String
+struct TokenRequiredQueryStruct {
+    token: String,
 }
 
 //工厂函数
 
 #[get("/alpha/{quest}")]
-async fn factory_alpha(web::Path(quest): web::Path<String>,info: web::Query<TokenRequiredQueryStruct>)->HttpResponse{
+async fn factory_alpha(
+    web::Path(quest): web::Path<String>,
+    info: web::Query<TokenRequiredQueryStruct>,
+) -> HttpResponse {
     //校验token
-    if &info.token!=TOKEN{
-        return return_error_query(String::from("Invalid token : ")+&info.token)
+    if &info.token != TOKEN {
+        return return_error_query(String::from("Invalid token : ") + &info.token);
     }
     return match &quest[..] {
         "version" => return_text_result(get_alpha_version()),
         "addr" => return_redirect_result(get_alpha_addr()),
-        _ => return_error_query(format!("/alpha/{}",quest))
-    }
+        _ => return_error_query(format!("/alpha/{}", quest)),
+    };
 }
 
 #[get("/info/{quest}")]
@@ -69,22 +75,22 @@ async fn factory_info(web::Path(quest): web::Path<String>) -> HttpResponse {
         "ventoy_plugin_addr" => {
             return_redirect_string(String::from(STATION_URL) + "/Socket/Hub/ventoy_wimboot.img")
         }
-        _ => return_error_query(quest)
+        _ => return_error_query(quest),
     };
 }
 
 #[get("/plugin/cateData")]
-async fn factory_plugin_cate() -> HttpResponse{
-    return return_json_result(get_plugin_cate())
+async fn factory_plugin_cate() -> HttpResponse {
+    return return_json_result(get_plugin_cate());
 }
 
 #[get("/plugin/listData")]
 async fn factory_plugin_list(info: web::Query<PluginListQueryStruct>) -> HttpResponse {
     //判断目录是否存在
-    if !Path::new(&(String::from(DISK_DIRECTORY)+"/插件包/"+&info.name.clone())).exists() {
-        return return_error_query(String::from("No such cate"))
+    if !Path::new(&(String::from(DISK_DIRECTORY) + "/插件包/" + &info.name.clone())).exists() {
+        return return_error_query(String::from("No such cate"));
     }
-    return return_json_result(get_plugin_list(info.name.clone()))
+    return return_json_result(get_plugin_list(info.name.clone()));
 }
 
 #[get("/ept/index")]
@@ -94,20 +100,22 @@ async fn factory_ept_index() -> HttpResponse {
 
 #[get("/ept/addr")]
 async fn factory_ept_addr(info: web::Query<EptAddrQueryStruct>) -> HttpResponse {
-    return return_redirect_string(get_ept_addr(EptAddrQueryStruct{
-        cate:info.cate.clone(),
-        name:info.name.clone(),
-        version:info.version.clone(),
-        author:info.author.clone()
+    return return_redirect_string(get_ept_addr(EptAddrQueryStruct {
+        cate: info.cate.clone(),
+        name: info.name.clone(),
+        version: info.version.clone(),
+        author: info.author.clone(),
     }));
 }
 
 #[get("/misc/{quest}")]
 async fn factory_misc(web::Path(quest): web::Path<String>) -> HttpResponse {
     return match &quest[..] {
-        "ariang" => return_redirect_string(String::from("https://www.edgeless.top/ariang/#!/settings/rpc/set/http/127.0.0.1/6800/jsonrpc")),
+        "ariang" => return_redirect_string(String::from(
+            "https://www.edgeless.top/ariang/#!/settings/rpc/set/http/127.0.0.1/6800/jsonrpc",
+        )),
         "sbl" => return_redirect_string(String::from("https://blog.gocrossthegfw.cf/")),
-        _ => return_error_query(quest)
+        _ => return_error_query(quest),
     };
 }
 
@@ -200,7 +208,7 @@ fn return_text_result(content: Result<String, String>) -> HttpResponse {
     }
     return HttpResponse::Ok().body(format!("{}", content.unwrap()));
 }
-fn return_text_result_gb(content:Result<String, String>)->HttpResponse{
+fn return_text_result_gb(content: Result<String, String>) -> HttpResponse {
     if let Err(error) = content {
         return return_error_internal(error);
     }
@@ -233,26 +241,25 @@ fn return_redirect_string(url: String) -> HttpResponse {
 }
 
 //按Json返回函数
-fn return_json_result<T: Serialize>(data:Result<T,String>) ->HttpResponse{
+fn return_json_result<T: Serialize>(data: Result<T, String>) -> HttpResponse {
     if let Err(error) = data {
         return return_error_internal(error);
     }
-    return HttpResponse::Ok()
-        .json(data.unwrap())
+    return HttpResponse::Ok().json(data.unwrap());
 }
 
 //返回内部错误
-fn return_error_internal(msg:String)->HttpResponse{
+fn return_error_internal(msg: String) -> HttpResponse {
     return HttpResponse::Ok()
         .status(StatusCode::INTERNAL_SERVER_ERROR)
-        .body(format!("Error: Internal\n{}",msg))
+        .body(format!("Error: Internal\n{}", msg));
 }
 
 //返回查询错误
-fn return_error_query(msg:String)->HttpResponse{
+fn return_error_query(msg: String) -> HttpResponse {
     return HttpResponse::Ok()
         .status(StatusCode::BAD_REQUEST)
-        .body(format!("Error: Quest\nUnknown quest:{}",msg))
+        .body(format!("Error: Quest\nUnknown quest:{}", msg));
 }
 
 //获取ISO版本号/info/iso_version
@@ -325,86 +332,88 @@ fn get_hub_addr() -> Result<String, String> {
 }
 
 //获取插件分类数组
-fn get_plugin_cate() -> Result<CateData,String>{
+fn get_plugin_cate() -> Result<CateData, String> {
     //扫描插件包目录
-    let cate_list=fs::read_dir(DISK_DIRECTORY.to_string()+"/插件包");
-    if let Err(_)=cate_list{
-        return Err(String::from("get_plugin_cate:Fail to read : ")+&DISK_DIRECTORY+"/插件包");
+    let cate_list = fs::read_dir(DISK_DIRECTORY.to_string() + "/插件包");
+    if let Err(_) = cate_list {
+        return Err(String::from("get_plugin_cate:Fail to read : ") + &DISK_DIRECTORY + "/插件包");
     }
 
     //形成Vec<String>
-    let mut result=Vec::new();
+    let mut result = Vec::new();
     for entry in cate_list.unwrap() {
         //解析node名称
         let file_name = entry.unwrap().file_name().clone();
         let true_name = file_name.to_str().unwrap();
         //判断是否为目录，是则push到Vector
-        let path=String::from(DISK_DIRECTORY)+"/插件包/"+&true_name;
-        if Path::new(&path).is_dir(){
+        let path = String::from(DISK_DIRECTORY) + "/插件包/" + &true_name;
+        if Path::new(&path).is_dir() {
             result.push(true_name.to_string());
         }
     }
     //println!("{:?}",result);
-    return Ok(CateData {
-        payload:result
-    });
+    return Ok(CateData { payload: result });
 }
 
 //获取分类详情
-fn get_plugin_list(cate_name:String) -> Result<ListData,String>{
+fn get_plugin_list(cate_name: String) -> Result<ListData, String> {
     //扫描分类目录
-    let list=fs::read_dir(DISK_DIRECTORY.to_string()+"/插件包/"+&cate_name);
-    if let Err(_)=list{
-        return Err(String::from("get_plugin_list:Can't open as directory : ")+&DISK_DIRECTORY+"/插件包/"+&cate_name);
+    let list = fs::read_dir(DISK_DIRECTORY.to_string() + "/插件包/" + &cate_name);
+    if let Err(_) = list {
+        return Err(String::from("get_plugin_list:Can't open as directory : ")
+            + &DISK_DIRECTORY
+            + "/插件包/"
+            + &cate_name);
     }
 
     //形成Vec<ListObj>
-    let mut result=Vec::new();
-    for entry in list.unwrap(){
+    let mut result = Vec::new();
+    for entry in list.unwrap() {
         //解析node名称
-        let dir_entry=entry.unwrap();
+        let dir_entry = entry.unwrap();
         let file_name = &dir_entry.file_name().clone();
         let true_name = file_name.to_str().unwrap().to_string();
 
         //获取文件大小
         let meta_data = fs::metadata(&dir_entry.path());
-        if let Err(_)=meta_data{
-            return Err(String::from("get_plugin_list:Fail to read : ")+&DISK_DIRECTORY+"/插件包/"+&cate_name);
+        if let Err(_) = meta_data {
+            return Err(String::from("get_plugin_list:Fail to read : ")
+                + &DISK_DIRECTORY
+                + "/插件包/"
+                + &cate_name);
         }
-        let file_size=meta_data.unwrap().file_size();
+        let file_size = meta_data.unwrap().file_size();
 
         //将后缀名为.7z的推入Vec
-        if regex::is_match(".7z",&true_name).unwrap(){
-            result.push(ListObj{
-                name:true_name.clone(),
-                size:file_size,
-                node_type:String::from("FILE"),
-                url:String::from(STATION_URL)+"/插件包/"+&true_name
+        if regex::is_match(".7z", &true_name).unwrap() {
+            result.push(ListObj {
+                name: true_name.clone(),
+                size: file_size,
+                node_type: String::from("FILE"),
+                url: String::from(STATION_URL) + "/插件包/" + &true_name,
             })
         }
     }
-    return Ok(ListData {
-        payload:result
-    });
+    return Ok(ListData { payload: result });
 }
 
 //生成ept索引
-fn get_ept_index()->Result<String,String>{
+fn get_ept_index() -> Result<String, String> {
     //获取分类
-    let cate_data=get_plugin_cate()?;
+    let cate_data = get_plugin_cate()?;
 
     //生成文本
-    let mut result=String::new();
-    for cate_name in cate_data.payload{
+    let mut result = String::new();
+    for cate_name in cate_data.payload {
         //对当前分类获取文件列表
         let list = get_plugin_list(cate_name.clone())?;
 
         //遍历列表，生成字段
-        for plugin_info in list.payload{
+        for plugin_info in list.payload {
             //去拓展名
-            let plugin_name=&plugin_info.name[0..plugin_info.name.len()-3];
+            let plugin_name = &plugin_info.name[0..plugin_info.name.len() - 3];
             //生成字段
-            let line=String::from(plugin_name)+"_"+&cate_name+"\n";
+            let line = String::from(plugin_name) + "_" + &cate_name + "\n";
             //添加字段
             result.push_str(&line);
         }
@@ -413,6 +422,15 @@ fn get_ept_index()->Result<String,String>{
 }
 
 //生成下载地址
-fn get_ept_addr(payload:EptAddrQueryStruct)->String{
-    return String::from(STATION_URL)+"/插件包/"+&payload.cate+"/"+&payload.name+"_"+&payload.version+"_"+&payload.author+".7z";
+fn get_ept_addr(payload: EptAddrQueryStruct) -> String {
+    return String::from(STATION_URL)
+        + "/插件包/"
+        + &payload.cate
+        + "/"
+        + &payload.name
+        + "_"
+        + &payload.version
+        + "_"
+        + &payload.author
+        + ".7z";
 }
